233c33655482b89cd882d68cd190dfb1cc864e26
Frag Spawn <frag.spawn@gmail.com>
2020-10-19 12:46:29 +1000

    updated dpeloyment script

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+mysql -u root -e 'drop database contovoris'
 mysql -u root -e 'create database contovoris'
 mysql -u root contovoris < api/sql/contovoris.sql
 
 php -S localhost:8888
+
+#react
+#cd reactapp
+#npm install
+#npm run build
+#mv dist ..
+
